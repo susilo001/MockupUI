fa26d4a17d7d97235eb27a1183f75b061bf7b208
susilo001 <susil.2000@students.amikom.ac.id>
2022-06-11 21:42:38 +0700

    Add Dark Theme

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,36 @@
-// Search Bar & Toggle
+import LazyLoad from "./lazyload.esm.js";
+import themeChange from './theme-change/index.js';
+import themeToggle from './theme-change/index.js';
 
-
-var btnShow = document.getElementById('btn-show')
 var searchBar = document.getElementById('search-bar');
-function show() {
-
+var btnshow = document.getElementById('btn-show');
+btnshow.addEventListener('click', function () {
     if (searchBar.classList.contains('hidden')) {
         searchBar.classList.remove('hidden')
         searchBar.classList.add('block')
 
     }
-}
-function hide() {
+})
+searchBar.addEventListener('mouseout', () => {
     searchBar.classList.remove('block')
     searchBar.classList.add('hidden')
-}
+})
+
+themeToggle()
+themeChange()
+
+// Lazy Load Plugins
+var lazyLoadInstance = new LazyLoad({
+    use_native: true,
+    callback_error: (img) => {
+        // Use the following line only if your images have the `srcset` attribute
+        img.setAttribute("srcset", "fallback_image@1x.jpg 1x, fallback_image@2x.jpg 2x");
+        img.setAttribute("src", "fallback_image@1x.jpg");
+    }
+});
+lazyLoadInstance.update();
+// Search Bar & Toggle
+
 
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     "./*.html",
     "./templates/*.html"
   ],
+  daisyui: {
+    styled: true,
+    themes: ["emerald", "dracula"],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dracula",
+    lightTheme: "emerald",
+  },
   theme: {
+    fontFamily: {
+      'content': ['Roboto', 'sans-serif', 'Regular-400'],
+      'title': ['Lato', 'sans-serif', 'Bold-700'],
+
+    },
     extend: {},
   },
   plugins: [require("daisyui"), require("autoprefixer")],
